4daa66d993a7e0a3d8fa81304a98f3b51c2df821
NJUVISION <62103699+NJUVISION@users.noreply.github.com>
2020-11-16 16:23:33 +0800

    Update main.m

--- main.m
+++ main.m
@@ -1,6 +1,6 @@
 imsrc=imread('6to7/6to7.png');
 imtgt=imread('6to7/7to6.png');
 im=imread('6to7/7.png');
-[map]=AHMpro(imtgt,imsrc);
+[map]=HHM(imtgt,imsrc);
 correct=PA(map,im);
 imwrite(correct,'6to7/output7test.png');
